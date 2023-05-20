886288244a07c663257bf2b1a457ed6a6862fa36
USERSAD\abarrenm <abarrenm@emeal.nttdata.com>
2023-05-20 19:47:41 +0200

    Add get dive diary from user

--- src/main/java/edu/uoc/abarrena/users/infrastructure/repository/mybatis/mapper/DiveDiaryMapper.java
+++ src/main/java/edu/uoc/abarrena/users/infrastructure/repository/mybatis/mapper/DiveDiaryMapper.java
@@ -1,10 +1,7 @@
 package edu.uoc.abarrena.users.infrastructure.repository.mybatis.mapper;
 
 import edu.uoc.abarrena.users.infrastructure.repository.mybatis.entity.DiveDiaryEntity;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -15,6 +12,19 @@ public interface DiveDiaryMapper {
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     public void save(DiveDiaryEntity diveDiaryEntity);
 
+    @Results(id = "diveDiaryResultMap", value = {
+        @Result(property = "id", column = "id"),
+        @Result(property = "date", column = "date"),
+        @Result(property = "time", column = "time"),
+        @Result(property = "spot", column = "spot"),
+        @Result(property = "maxDepth", column = "max_depth"),
+        @Result(property = "temperature", column = "temperature"),
+        @Result(property = "visibility", column = "visibility"),
+        @Result(property = "o2Percentage", column = "o2_percentage"),
+        @Result(property = "airIn", column = "air_in"),
+        @Result(property = "airOut", column = "air_out"),
+        @Result(property = "comment", column = "comment")
+    })
     @Select("SELECT * FROM DIVE_DIARY WHERE TRAVELER_ID = #{id}")
     public List<DiveDiaryEntity> findByUserId(Long id);
 }
